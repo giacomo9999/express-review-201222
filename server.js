fcdc3c1bd873d80a66e4732ce42ba00f2ad398a7
giacomo9999 <gmarchesi888@gmail.com>
2020-12-22 14:33:36 -0500

    Moving on to the next thing...

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+const express = require("express");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const app = express();
+
+MongoClient.connect("mongo-connection-string", (err, client) => {});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+  console.log("Listening on 3000");
+});
+
+app.get("/", (req, res) => {
+  console.log("GET Request received for page...", __dirname);
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/quotes", (req, res) => {
+  console.log("POST request received...", req.body);
+});
